d1d2b7e938ae1a371c95ddcbb49638858203a2d7
dani <dani.moreno192003@gmail.com>
2022-10-13 09:37:48 +0200

    Ejercicio2

---
+++ 102DMM-02.js
@@ -0,0 +1,10 @@
+let contactos=
+    [{nombre:'Maxwell Wright',telefono:'(0191) 719 6495',correo:'Curabitur.egestas.nunc@nonummyar.co.uk'}
+    ,{nombre:'Raja Villareal',telefono:'0866 398 2895',correo:'posuere.vulputate@sed.com'}
+    ,{nombre:'Helen Richards',telefono:'0800 1111',correo:'libero@convallis.edu'}];
+
+    contactos.push({nombre:'Masisie Haley',telefono:'0913 531 3030',correo:'risus.Quisque@urna.ca'})
+
+
+console.log(contactos[0]);
+console.log(contactos[contactos.length-1]);
