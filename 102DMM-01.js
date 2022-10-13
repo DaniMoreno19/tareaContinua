dd112da7b5783dcc3677ebe61e159f27de58f472
dani <dani.moreno192003@gmail.com>
2022-10-13 09:35:16 +0200

    Ejercicio1

---
+++ 102DMM-01.js
@@ -0,0 +1,14 @@
+        let nombre1="Maxwell Wright";
+        let telefono1="(0191) 719 6495";
+        let correo1="Curabitur.egestas.nunc@nonummyar.co.uk";
+
+        let nombre2="Raja Villareal";
+        let telefono2="0866 398 2895";
+        let correo2="posuere.vulputate@sed.com";
+
+        let nombre3="Helen Richards";
+        let telefono3="0800 1111";
+        let correo3="libero@convallis.edu";
+
+        console.log(nombre1+"/"+telefono1+"/"+correo1);
+        console.log(nombre3+"/"+telefono3+"/"+correo3);
